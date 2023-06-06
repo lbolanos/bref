cb5c4336c6c65c13e6a6a52493d1f754325e7cd0
Hector Leonardo Bolaños Muñoz <lbolanos@outlook.com>
2023-06-06 16:43:40 -0500

    commit all

--- src/Runtime/LambdaRuntime.php
+++ src/Runtime/LambdaRuntime.php
@@ -90,7 +90,7 @@ final class LambdaRuntime
             $result = $this->invoker->invoke($handler, $event, $context);
 
             $this->sendResponse($context->getAwsRequestId(), $result);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             $this->signalFailure($context->getAwsRequestId(), $e, $event);
 
             return false;
